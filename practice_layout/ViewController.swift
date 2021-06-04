1642fcfd9d2573dab5bb55c7720fa42c7c9f1500
kokikudo <massanmesu@icloud.com>
2021-06-04 15:05:19 +0900

    Initial Commit

---
+++ practice_layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_layout
+//
+//  Created by kudo koki on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
